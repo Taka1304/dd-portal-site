a076976a034c9c0a2c29f8aa7bbe65e8c8434433
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-02-12 00:46:55 +0900

    Add: Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="h-12 flex items-center justify-between p-4">
+			<p>&copy; 2024 Data Dreamers</p>
+		</footer>
+	);
+};
+
+export default Footer;
