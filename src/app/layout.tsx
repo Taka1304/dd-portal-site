1296307c2716a71bb772dc8af8e4a4d3108bff91
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-02-12 00:48:10 +0900

    Add: layout.tsxにHeaderとFooterを統合

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "~/components/layout/Footer";
+import Header from "~/components/layout/Header";
 import "./globals.css";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Home - Data Dreamers",
-  description: "Data Dreamers",
+	title: "Data Dreamers",
+	description: "Data Dreamers",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<body className={inter.className}>
+				<Providers>
+					<div className="flex flex-col min-h-screen w-screen justify-between">
+						<Header />
+						<main className="grow flex flex-col items-center justify-between">
+							{children}
+						</main>
+						<Footer />
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
