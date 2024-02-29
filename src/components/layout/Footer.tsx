92643e8bf3d22df53ea839d6e080f7560d9dca3a
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-02-29 11:32:12 +0900

    add: footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,32 @@
+import { Box, Flex, Heading, Text } from "@yamada-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="h-12 flex items-center justify-between p-4">
-			<p>&copy; 2024 Data Dreamers</p>
-		</footer>
+		<Box as={"footer"} p={4} w={"full"} h={"60"} bgGradient={"blue"}>
+			<Flex
+				as={Link}
+				direction="row"
+				href={"/"}
+				alignItems={"center"}
+				w={"full"}
+			>
+				<Image
+					src="/images/icon-removebg.png"
+					alt="Data Dreamers"
+					width={50}
+					height={50}
+				/>
+				<Heading as={"h1"} fontWeight={"bold"} color={"white"} fontSize={"2xl"}>
+					Data Dreamers
+				</Heading>
+			</Flex>
+			<Text variant={""} fontSize="sm" color={"white"}>
+				&copy; 2024 Data Dreamers. All Rights Reserved.
+			</Text>
+		</Box>
 	);
 };
 
