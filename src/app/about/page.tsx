1dbf79c239b348fa8f64577ac52eb5763d5473a1
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-23 14:54:50 +0900

    Add: HeroImage component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,20 @@
 import { Container, Flex, Heading, Text } from "@yamada-ui/react";
+import HeroImage from "~/components/layout/HeroImage";
+
+const Images = {
+	src: "/images/challenge-lab-01.avif",
+	alt: "Data Dreamers",
+	priority: true,
+};
 
 export default function Home() {
 	return (
 		<Flex direction={"column"} align={"center"} justify={"space-between"}>
+			<HeroImage {...Images}>
+				昨日より良い明日を
+				<br />
+				&emsp;データサイエンスで。
+			</HeroImage>
 			<Container maxW={"4xl"}>
 				<Heading>About</Heading>
 				<Heading as={"h2"} size={"md"}>
