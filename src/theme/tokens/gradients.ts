24fdce671f565875faa29881c65acd062cdd2d1f
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-25 09:45:26 +0900

    Add: blackAlpha gradient

--- src/theme/tokens/gradients.ts
+++ src/theme/tokens/gradients.ts
@@ -3,4 +3,7 @@ import { ThemeTokens } from "@yamada-ui/react";
 export const gradients: ThemeTokens = {
 	blue: "linear(to-br, #0E416B, #000029)",
 	white: "linear(to-r, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);",
+	black: "linear(to-r, #000000 0%, #434343 100%)",
+	blackAlpha:
+		"linear-gradient(to-b, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .10) 40%)",
 };
