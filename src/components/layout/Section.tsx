7a22317bb76d3d18f79f050579eef9b46bad42ad
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-27 00:34:31 +0900

    Add: about view

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, FlexProps } from "@yamada-ui/react";
+import { Container, Flex, FlexProps } from "@yamada-ui/react";
 import React, { FC } from "react";
 
 type Props = {
@@ -9,10 +9,8 @@ type Props = {
 const Section: FC<Props> = ({ parent, children }) => {
 	return (
 		<Flex w={"full"} justify={"center"} {...parent}>
-			<Container maxW="4xl" py="20">
-				<Box gapY={20} display={"contents"}>
-					{children}
-				</Box>
+			<Container maxW="4xl" p="4rem 2rem">
+				{children}
 			</Container>
 		</Flex>
 	);
