7d68b5c51bb6023544c016ba345e49da3ad3186d
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-31 15:55:52 +0900

    Add: news

---
+++ src/constants/news.ts
@@ -0,0 +1,38 @@
+type Content = {
+	date: string;
+	title: string;
+	link: string;
+};
+
+export const NEWS: Content[] = [
+	{
+		date: "2024/3/8",
+		title:
+			"データサイエンティスト育成団体が設立1年目で大学生向けハッカソン優勝の秘訣をお伺いしました",
+		link: "https://data-viz-lab.com/kanazawa-univ",
+	},
+	{
+		date: "2024/2/9",
+		title:
+			"【金沢工業大学】データサイエンス系課外プロジェクト「Data Dreamers」とは？",
+		link: "https://ds100.jp/future/s-017/",
+	},
+	{
+		date: "2024/1/27",
+		title:
+			"【金沢工業大学】研究夢考房Start Up Project　Data Dreamersが「Azure OpenAI Service 大学生向けハッカソン」で優勝",
+		link: "https://ds100.jp/news/2024/02/02/【金沢工業大学】研究夢考房start-up-project　data-dreamersが「azure-openai-service/",
+	},
+	{
+		date: "2023/12/20",
+		title:
+			"金沢工業大学初のデータサイエンスに関する課外プロジェクトが2023年度に発足",
+		link: "https://kyodonewsprwire.jp/release/202312204590",
+	},
+	{
+		date: "2022/12/14",
+		title:
+			"4ヵ月NTT西日本の“社員”となった学生たちがコーオプ教育の成果を発表。大学で学んだデータサイエンスの知識を活かし、問題解決に挑む",
+		link: "https://www.kanazawa-it.ac.jp/kitnews/2022/1214_coop.html",
+	},
+];
