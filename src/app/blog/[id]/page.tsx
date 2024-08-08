9302fdb1a269a191ab9f260ba2c51214c0f45f57
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-08-08 23:25:59 +0900

    fix: 詳細ページのエラー修正

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Heading, VStack } from "@yamada-ui/react";
+import React from "react";
+
+const BlogContentPage = () => {
+	return (
+		<VStack>
+			<Heading>記事詳細</Heading>
+		</VStack>
+	);
+};
+
+export default BlogContentPage;
